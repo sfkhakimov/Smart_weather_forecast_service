8a8f689c30eba71dec4b8bd70ed95916552a848b
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-04-11 20:07:50 +0300

    Updates

---
+++ block/api/Api.js
@@ -0,0 +1,15 @@
+export default class Api {
+  constructor(options) {
+    this.options = options;
+  }
+
+  getWeather(city) {
+    return fetch(`${this.options.baseUrl}?q=${city}&${this.options.metric}&${this.options.id}&${this.options.lang}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .catch((err) => err);
+  }
+}
